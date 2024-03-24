34b9b35b013d06064c10430aacdd6c5cb994b6f9
javanmardi <javanmardi.ej@gmail.com>
2024-03-24 12:42:12 +0330

    feat: initialize the project

---
+++ OnlineStore/Areas/Admin/Controllers/ProductTypesController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OnlineStore.Data;
+using OnlineStore.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace OnlineStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductTypesController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public ProductTypesController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(_db.ProductTypes.ToList());
+        }
+
+        //Get Create Action Method
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //Post Create Action Method
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(ProductTypes productTypes)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(productTypes);
+                await _db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+                //return RedirectToAction("Index");
+            }
+            return View(productTypes);
+        }
+    }
+}
